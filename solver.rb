a16434740e336bab2b2c280cd5cc333a60d1b07f
Munish <hmunish5@gmail.com>
2023-09-06 19:25:59 +0200

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,14 +1,18 @@
 class Solver
-    def factorial(i)
-        if(i < 0) then raise Exception.new("Please enter positive numbers")
-        else
-            fac = 1
-            c = 1
-            while c <= i
-                fac = fac * c
-                c = c+1
-            end
-            return fac
-        end
+  def factorial(num)
+    raise Exception, 'Please enter positive numbers' if num.negative?
+
+
+    fac = 1
+    c = 1
+    while c <= num
+      fac *= c
+      c += 1
     end
+    fac
+  end
+
+  def reverse(word)
+    word.reverse
+  end
 end
