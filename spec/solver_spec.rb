8322354b4c307792a7ebd7e0c65ee1e1705b5be8
Munish <hmunish5@gmail.com>
2023-09-06 19:01:36 +0200

    Add test to check factorial method returns correct output

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -15,5 +15,9 @@ describe Solver do
       it 'should return 1' do
         expect(solver.factorial(0)).to eq 1
       end
+
+      it 'should return 120' do
+        expect(solver.factorial(5)).to eq 120
+      end
     end
   end
